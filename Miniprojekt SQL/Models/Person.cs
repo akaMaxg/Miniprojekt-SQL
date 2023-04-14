7f41b5b2fee4a1f8e0a4895f19e8b97c78e2f674
akaMaxg <m.guclu@live.se>
2023-04-14 17:45:37 +0200

    Created Models subfolder with classes that represented the tables in DB.

---
+++ Miniprojekt SQL/Models/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace Miniprojekt_SQL.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string PersonName { get; set; }
+    }
+}
---
+++ Miniprojekt SQL/Models/ProjectPerson.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miniprojekt_SQL.Models
+{    public class ProjectPerson
+    {
+        public int Id { get; set; }
+        public int ProjectId { get; set; }
+        public int PersonId { get; set; }
+        public int Hours { get; set; }
+    }
+}
